dcb4e55fc86beba5dcca4007a01016b71a443f45
Peter Schrammel <peter.schrammel@experteer.com>
2014-02-12 13:27:20 +0100

    updated homepage url

--- lib/experella-proxy/version.rb
+++ lib/experella-proxy/version.rb
@@ -1,5 +1,7 @@
 module ExperellaProxy
   # ExperellaProxy Gemversion
+  # 0.0.6
+  # * updated homepage
   # 0.0.5
   # * added :host_port option to backend configuration
   # 0.0.4
@@ -9,5 +11,5 @@ module ExperellaProxy
   # * added self-signed SSL certificate for TLS/HTTPS
   # * added config template init functionality
   #
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
